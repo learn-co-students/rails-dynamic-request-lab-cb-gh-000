bbe207742358947913ff6b1211b9c8a51d658c2e
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-18 22:57:59 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,8 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id]) #return the post with the given id (id is a hash key)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+  resources :students, only: :show
+  # get 'posts/:id', to: 'posts#show'
+  #SAME AS
+  # resources :posts, only: :show
 end
